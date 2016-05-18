1aeff5a5c614ea1f4094f8f0508c809f647d954a
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-05-18 21:38:05 +0200

    Remove useless code

--- src/test/java/demo/tests/GoogleSerenityDriverIT.java
+++ src/test/java/demo/tests/GoogleSerenityDriverIT.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 @RunWith(SerenityRunner.class)
 public class GoogleSerenityDriverIT {
@@ -30,29 +29,5 @@ public class GoogleSerenityDriverIT {
 
         // Check the title of the page
         System.out.println("Page title is: " + webdriver.getTitle());
-    }
-
-    public static void LegazyWebDriverTest(String[] args) {
-        // Create a new instance of the html unit driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver = new HtmlUnitDriver();
-
-        // And now use this to visit Google
-        driver.get("http://www.google.com");
-
-        // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
-
-        // Enter something to search for
-        element.sendKeys("Cheese!");
-
-        // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
-
-        // Check the title of the page
-        System.out.println("Page title is: " + driver.getTitle());
-
-        driver.quit();
     }
 }
